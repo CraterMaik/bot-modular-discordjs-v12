567717fb464d5f0fb81689b164a40d74486e3850
Crater <33011158+CraterMaik@users.noreply.github.com>
2020-09-26 17:22:24 -0500

    Categorie Music

--- config.js
+++ config.js
@@ -2,11 +2,13 @@ module.exports = {
  dirBase : './database/base_1.db',
  superusers: [], //Puedes añadir ID
  token: 'TOKEN-HERE',
+ ytKey: 'YT-KEY'
  prefix: '?',
  statusBOT: 'Estoy listo!',
  categories: [
   {name: "general", priority: 5},
-  {name: "admin", priority: 8}
+  {name: "admin", priority: 8},
+  {name: "music", priority: 7}
  ],
  groups: [
   {name: "User", permlvl: 0},
